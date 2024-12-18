8fb362781467802cbc7222fb7dfa853b9ddd1cef
cornelia247 <ekpenyongcornelia2@gmail.com>
2024-12-18 13:16:11 +0100

    adding diagrams

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -6,12 +6,11 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "spring-time-terraform-backend-bucket"
-    region         = "us-east-1"
-    key            = "newterraform/state"
-    dynamodb_table = "terraform-lock-table"
-
-  }
+    bucket         = "" # Example: "spring-time-terraform-backend-bucket". Name of the S3 bucket for state storage.
+    region         = "" # Example: "us-east-1". AWS region where the S3 bucket is located.
+    key            = "" # Example: "newterraform/state". Path to store the Terraform state file in the bucket.
+    dynamodb_table = "" # Example: "terraform-lock-table". Name of the DynamoDB table for state locking.
+}
 }
 
 provider "aws" {
